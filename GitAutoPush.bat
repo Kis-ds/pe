90ef337eed941ef7f1351c84f01ca4cb44c33906
Kis-ds <datasolution@koreainvestment.com>
2023-06-27 10:50:50 +0900

    RPA pickcle push 2023-06-27

--- GitAutoPush.bat
+++ GitAutoPush.bat
@@ -1,30 +1,13 @@
-:loop
-	:: Navigate to the directory you wish to push to GitHub
-	::Change <path> as needed. Eg. C:\Users\pookie\Desktop\Writings
-	cd D:\git\pe\pickle
+cd D:\git\pe\pickle 
 
-	::Initialize GitHub
-	git init
-	
-	::Pull any external changes (maybe you deleted a file from your repo?)
-	git pull
-	
-	::Add all files in the directory
-	git add --all
-	
-	::Commit all changes with the message "auto push". 
-	::Change as needed.
-	git commit -m "RPA pickcle push "%date%
-	
-	::Push all changes to GitHub 
-	git push
-	
-	::Alert user to script completion and relaunch.
-	echo Complete. Relaunching...
-	
-	::Wait 3600 seconds until going to the start of the loop.
-	::Change as needed.
-	TIMEOUT 3600
-	
-::Restart from the top.	
-goto loop
+git init
+
+git status
+
+git add *
+
+git commit -m "RPA pickcle push "%date%
+
+git remote add origin https://github.com/Kis-ds/pe.git
+
+git push origin master
