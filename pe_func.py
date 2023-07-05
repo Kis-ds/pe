ebd370e41560eea62ebf434f37e6ffedcb32a261
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-07-05 16:51:55 +0900

    Add files via upload

--- pe_func.py
+++ pe_func.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import warnings
 import pandas as pd
+import numpy as np
 import json
 import pickle
 import requests
@@ -104,6 +105,9 @@ def get_rcept_no_by_corp(corp_code, report_nm, bgn_de, end_de):
 def get_mezn_data(knd, corp_nm, start_dt, end_dt, intr_ex_min, intr_ex_max, intr_sf_min, intr_sf_max):
     with open('./pickle/Mezzanine_new.pkl', 'rb') as f:
         df = pickle.load(f)
+        df['발행사'] = df['발행사'].str.replace('주식회사', '').str.replace('(주)', '').str.replace('㈜', '').str.replace('(',
+                                                                                                              '').str.replace(
+            ')', '').str.strip()
         df = df[df['종류'].isin(knd)]
         df['표면이자율(%)'] = df['표면이자율(%)'].str.strip()
         df['만기이자율(%)'] = df['만기이자율(%)'].str.strip()
@@ -249,19 +253,18 @@ def get_perp_docu(rcept_no):
             prft_rate = table.find('te', attrs={'acode':'PRFT_RATE'}).get_text() # 표면이자율
             lst_rtn_rt = table.find('te', attrs={'acode':'LST_RTN_RT'}).get_text() # 만기이자율
             exp_dt = table.find('tu', attrs={'aunit': 'EXP_DT'}).get_text()  # 사채만기일
-            exp_dt = '' if exp_dt == '-' else exp_dt
             exp_dt_dur = table.find('te', attrs={'acode': 'EXP_DT_DUR'}).get_text()  # 사채만기기간
             int_gv_mth = table.find('te', attrs={'acode': 'INT_GV_MTH'}).get_text().replace('\n', '').replace('-', '')  # 이자지급방법
-            int_stp = table.find('te', attrs={'acode': 'INT_STP'}).get_text().replace('\n', '').replace('-', '')  # 이자지급 정지(유예) 가능여부 및 조건
+            int_stp = table.find('te', attrs={'acode': 'INT_STP'}).get_text().replace('\n', '').replace('-', '').replace('&cr', '')  # 이자지급 정지(유예) 가능여부 및 조건
             int_stp_acm = table.find('te', attrs={'acode': 'INT_STP_ACM'}).get_text().replace('\n', '').replace('-', '')  # 유예이자 누적 여부
             int_st_up = table.find('te', attrs={'acode': 'INT_ST_UP'}).get_text().replace('\n',
-                                                                                          '').replace('-', '')  # 금리상향조정 등 이자율 조정 조건
-            rtn_mth = table.find('te', attrs={'acode': 'RTN_MTH'}).get_text().replace('\n', '').replace('-', '')  # 원금 만기상환방법
+                                                                                          '').replace('-', '').replace('&cr', '')  # 금리상향조정 등 이자율 조정 조건
+            rtn_mth = table.find('te', attrs={'acode': 'RTN_MTH'}).get_text().replace('\n', '').replace('-', '').replace('&cr', '')  # 원금 만기상환방법
             erl_rtn_mth = table.find('te', attrs={'acode': 'ERL_RTN_MTH'}).get_text().replace('\n',
-                                                                                              '').replace('-', '')  # 원금 조기상환 가능시점 및 조건
+                                                                                              '').replace('-', '').replace('&cr', '')  # 원금 조기상환 가능시점 및 조건
             exp_rnw_mth = table.find('te', attrs={'acode': 'EXP_RNW_MTH'}).get_text().replace('\n',
-                                                                                              '').replace('-', '')  # 원금 상환 만기연장 조건 및 방법
-            opt_fct = table.find('te', attrs={'acode': 'OPT_FCT'}).get_text().replace('\n', '').replace('-', '')  # 옵션에 관한 사항
+                                                                                              '').replace('-', '').replace('&cr', '')  # 원금 상환 만기연장 조건 및 방법
+            opt_fct = table.find('te', attrs={'acode': 'OPT_FCT'}).get_text().replace('\n', '').replace('-', '').replace('&cr', '')  # 옵션에 관한 사항
             chf_agn = table.find('te', attrs={'acode': 'CHF_AGN'}).get_text()  # 대표주관회사
             issu_table_group = soup.find('table-group', attrs={'aclass': 'CRP_ISSU'})  # 인수인(특정인에 대한 대상자별 사채발행내역)
             issu_table = issu_table_group.find('table', attrs={'aclass': 'EXTRACTION'})
@@ -274,7 +277,7 @@ def get_perp_docu(rcept_no):
             issu_nm = issu_nm[:-1]
 
             row = {'발행사': company_nm, '종류': pl_knd, '공시일': rcept_dt, '권면총액': dnm_sum, '자금조달의 목적': fnd,
-                   '표면이자율(%)': prft_rate, '만기이자율(%)': lst_rtn_rt, '사채만기일(기간)': exp_dt + "(" + exp_dt_dur + ")",
+                   '표면이자율(%)': prft_rate, '만기이자율(%)': lst_rtn_rt, '사채만기일': exp_dt,  "사채만기기간": exp_dt_dur,
                    '이자지급방법': int_gv_mth, '이자지급 정지(유예) 가능여부 및 조건': int_stp, '유예이자 누적여부': int_stp_acm, '이자율 조정 조건': int_st_up,
                    '원금 만기상환방법': rtn_mth, '원금 조기상환 조건': erl_rtn_mth, '원금 만기연장 조건 및 방법': exp_rnw_mth,
                    '옵션': opt_fct, '대표주관회사': chf_agn, '인수인': issu_nm}
@@ -285,6 +288,7 @@ def get_perp_docu(rcept_no):
     except Exception as e:
         print(rcept_no + " Error!")
         print(e)
+        row = {}
 
     return row
 
@@ -299,6 +303,17 @@ def get_cps_data(start_dt, end_dt, corp_nm):
                 & (df['발행사'] == corp_nm)]
     if df.empty == False :
         df = df.dropna(subset=['발행사'])
+        df.loc[df['전환청구기간']=='-', '전환청구기간'] = '-~-'
+        df[['전환청구시작일', '전환청구종료일']] = df['전환청구기간'].str.split('~', expand=True)
+        df['전환청구시작일'] = df['전환청구시작일'].str.replace(pat=r'[ㄱ-ㅣ가-힣]+', repl=r'', regex=True).str.replace(' ', '').str.replace('.', '')
+        df['전환청구종료일'] = df['전환청구종료일'].str.replace(pat=r'[ㄱ-ㅣ가-힣]+', repl=r'', regex=True).str.replace(' ',
+                                                                                                      '').str.replace(
+            '.', '')
+        df = df.rename(columns={'전환조건':'전환가액 조정에 관한 사항'})
+        df = df[['발행사', '공시일', '신주의 종류와 수', '1주당 액면가액', '증자방식', '전환비율', '전환가액', '전환가액결정방법',
+                  '전환주식종류', '전환주식수', '주식총수대비비율', '전환청구시작일', '전환청구종료일',
+                 '전환가액 조정에 관한 사항', '최저조정가액', '최저조정가액근거', '전환가액의 70%미만으로 조정가능한 잔여발행한도', '의결권',
+                 '옵션', '이익배당', '신주발행가액', '할인율 또는 할증율(%)']]
     return df
 
 # 주요사항보고서(유상증자결정) 상세정보 추출
@@ -342,6 +357,15 @@ def get_cps_docu(rcept_no):
             cvt_end_dt = table.find('tu', attrs={'aunit': 'CVT_END_DT'}).get_text()  # 종료일
             cvt_prd = cvt_bgn_dt + "~" + cvt_end_dt
             cvt_cdt = table.find('te', attrs={'acode': 'EXE_REG'}).get_text()  # 전환조건
+            try:
+                min_prc = table.find('te', attrs={'acode': 'MIN_PRC'}).get_text() # 최저 조정가액
+                min_rsn = table.find('te', attrs={'acode': 'MIN_RSN'}).get_text() # 최저 조정가액 근거
+                ctr_lmt = table.find('te', attrs={'acode': 'CTR_LMT'}).get_text() # 발행당시 전환가액의 70%미만으로 조정가능한 잔여발행한도
+            except:
+                min_prc = '-'
+                min_rsn = '-'
+                ctr_lmt = '-'
+            opt_fct = table.find('te', attrs={'acode': 'OPT_FCT'}).get_text() # 옵션에 관한 사항
             vtr_info = table.find('te', attrs={'acode': 'VTR_INFO'}).get_text()  # 의결권
             dvd_info = table.find('te', attrs={'acode': 'DVD_INFO'}).get_text()  # 이익배당
 
@@ -351,8 +375,9 @@ def get_cps_docu(rcept_no):
 
             row = {'발행사': company_nm, '공시일': rcept_dt, '신주의 종류와 수': pst_cnt, '1주당 액면가액': fval, '증자방식': ci_mth,
                    '전환비율': exe_rt, '전환가액': exe_prc, '전환가액결정방법': exe_func, '전환주식종류': cvt_knd, '전환주식수': cvt_cnt,
-                   '주식총수대비비율': cvt_rt, '전환청구기간': cvt_prd, '전환조건': cvt_cdt, '의결권': vtr_info, '이익배당': dvd_info,
-                   '신주발행가액': pst_iss_val, '할인율 또는 할증율(%)': dc_rate}
+                   '주식총수대비비율': cvt_rt, '전환청구기간': cvt_prd, '전환조건': cvt_cdt, '최저조정가액': min_prc, '최저조정가액근거': min_rsn,
+                   '전환가액의 70%미만으로 조정가능한 잔여발행한도': ctr_lmt, '의결권': vtr_info, '옵션':opt_fct,
+                   '이익배당': dvd_info, '신주발행가액': pst_iss_val, '할인율 또는 할증율(%)': dc_rate}
 
     except Exception as e:
         print(rcept_no + " Error!")
@@ -360,6 +385,34 @@ def get_cps_docu(rcept_no):
         row = {}
 
     return row
+
+# 메자닌채권 Data Cleansing
+def cleansing_mzn_df(df):
+    df['발행사'] = df['발행사'].str.replace('주식회사', '').str.replace('(주)', '').str.replace('㈜', '').str.replace('(',
+                                                                                                          '').str.replace(
+        ')', '').str.strip()
+    df['발행일'] = df['발행일'].str.replace(pat=r'[ㄱ-ㅣ가-힣]+', repl=r'', regex=True).str.replace(' ', '')
+    df['사채만기일'] = df['사채만기일'].str.replace(pat=r'[ㄱ-ㅣ가-힣]+', repl=r'', regex=True).str.replace(' ', '').str.replace('.','')
+    df = df[(df['발행일'] != '-') & (df['사채만기일'] != '-')]
+    df['만기기간'] = round((pd.to_datetime(df['사채만기일']) - pd.to_datetime(df['발행일']))/np.timedelta64(1, 'Y'), 0)
+    df['발행연도'] = df['발행일'].str[:4]
+    df['발행월'] = df['발행일'].str[4:6]
+    df = df.astype({'발행연도': int, '발행월': int})
+    df['권면총액'] = df['권면총액'].str.replace(',', '').replace('-', '0').str.replace('\n', '').astype('float')
+    df['표면이자율(%)'] = df['표면이자율(%)'].str.replace('\n', '').str.replace('&cr', '')
+    df['만기이자율(%)'] = df['만기이자율(%)'].str.replace('\n', '').str.replace('&cr', '')
+    df = df[(df['표면이자율(%)'] != '-') & (df['만기이자율(%)'] != '-')]
+    df['표면이자율(%)'] = df['표면이자율(%)'].astype('float')
+    df['만기이자율(%)'] = df['만기이자율(%)'].astype('float')
+    df['주식수'] = df['주식수'].str.replace(',', '').str.replace('\n', '').str.replace('&cr', '')
+    df = df[df['주식수'] != '-']
+    df['주식수'] = df['주식수'].astype('float')
+    df.loc[df['종류'] == '신주인수권', '종류'] = '신주인수권부사채권'
+    df['발행일'] = pd.to_datetime(df['발행일'])
+    df['공시일'] = pd.to_datetime(df['공시일'])
+    df = df.reset_index(drop=True)
+    df = df.loc[df.groupby(['종류', '발행사', '회차'])['공시일'].idxmax()]
+    return df
 
 # Dataframe 변환 및 다운로드
 def set_df(df, file_nm, start_dt, end_dt):
